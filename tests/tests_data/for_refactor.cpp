8b61eb585fdd30399da35b619d4945dc6a11fd19
Andrey Sozonov <andreysoz@yandex.ru>
2025-08-18 22:05:22 +0300

    init

---
+++ tests/tests_data/for_refactor.cpp
@@ -0,0 +1,31 @@
+#include <vector>  
+#include <iostream>  
+#include <print>
+
+class Base {  
+public:  
+  virtual void method() = 0;
+  ~Base() {}
+};   
+
+class Derived : public Base {  
+public:  
+  virtual void method() {};
+  virtual ~Derived() {} 
+};
+
+class MyType {
+public:
+    int id;
+    std::string name;
+    MyType(int i, const std::string& n) : id(i), name(n) {}
+};
+
+void foo() {  
+  std::vector<int> v1;  
+  for (const auto x1 : v1) {
+  }  
+  std::vector<MyType> vec = {{1, "obj1"}, {2, "obj2"}, {3, "obj3"}};
+  for (const auto x12 : vec) { 
+  }
+}  
